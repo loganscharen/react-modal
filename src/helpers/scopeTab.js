2eaf4e25ee22d022d5a521f9ccf742408d0bc443
loganscharen <61216565+loganscharen@users.noreply.github.com>
2024-03-22 07:55:43 -0500

    Update scopeTab.js to account for radio groups

--- src/helpers/scopeTab.js
+++ src/helpers/scopeTab.js
@@ -1,5 +1,15 @@
 import findTabbable from "./tabbable";
 
+function getActiveElement(el = document) {
+  return el.activeElement.shadowRoot
+    ? getActiveElement(el.activeElement.shadowRoot)
+    : el.activeElement;
+}
+
+function areFromSameRadioGroup(a, b) {
+  return a.name === b.name && a.type === "radio" && b.type === "radio";
+}
+
 export default function scopeTab(node, event) {
   const tabbable = findTabbable(node);
 
@@ -14,19 +24,23 @@ export default function scopeTab(node, event) {
   const shiftKey = event.shiftKey;
   const head = tabbable[0];
   const tail = tabbable[tabbable.length - 1];
+  const activeElement = getActiveElement();
 
   // proceed with default browser behavior on tab.
   // Focus on last element on shift + tab.
-  if (node === document.activeElement) {
+  if (node === activeElement) {
     if (!shiftKey) return;
     target = tail;
   }
 
-  if (tail === document.activeElement && !shiftKey) {
+  if (
+    (tail === activeElement || areFromSameRadioGroup(activeElement, tail)) &&
+    !shiftKey
+  ) {
     target = head;
   }
 
-  if (head === document.activeElement && shiftKey) {
+  if (head === activeElement && shiftKey) {
     target = tail;
   }
 
@@ -57,7 +71,7 @@ export default function scopeTab(node, event) {
   // the focus
   if (!isSafariDesktop) return;
 
-  var x = tabbable.indexOf(document.activeElement);
+  var x = tabbable.indexOf(activeElement);
 
   if (x > -1) {
     x += shiftKey ? -1 : 1;
